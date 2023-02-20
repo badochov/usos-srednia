cc926cb005a3dcb3ea087142982616f61f853976
Hubert Badocha <badocha.hubert@gmail.com>
2023-02-20 23:40:51 +0100

    Fixed GPA

--- src/content/v6.8.1/handler.ts
+++ src/content/v6.8.1/handler.ts
@@ -15,33 +15,39 @@ export class Usos6_8_1Handler extends Usos6_8_0Handler {
     switch (cell.childElementCount) {
       case 0: {
         const txt = cell.firstChild?.textContent?.trim() ?? ''
-        const split = txt.split(' ')
+        const split = txt.split('[')
         const name = split[0]
-        let code: string | null = null
-        if (split.length === 2) {
-          // FIXME I'm preetty sure it's a bug but I don't have any way of testing it.
-          code = split[0].split(']')[0]
-        }
+        const code = split.at(1)?.split(']').at(0)?.trim() ?? null
+
         return { name, code }
       }
       case 1: {
+        const codeSpan = cell.lastElementChild?.firstElementChild
+        if (codeSpan?.nodeName === "SPAN") {
+          // POLON
+          const name = cell.firstChild?.textContent?.trim() ?? ''
+          const code = codeSpan.textContent?.trim() ?? null
+
+          return { name, code }
+        }
+        // grades
         const name = cell.firstElementChild?.textContent?.trim() ?? ''
         const codeHelper = cell.lastChild?.textContent?.trim()
-        let code : string | null = null 
+        let code: string | null = null
         if (codeHelper !== undefined) {
-          code = codeHelper.substring(1, codeHelper.length - 1) //  // remove square brackets
+          code = codeHelper.substring(1, codeHelper.length - 1) // remove square brackets
         }
-  
         return { name, code }
       }
       case 2: {
+        // Used in linkages.
         const name = cell.firstElementChild?.textContent?.trim() ?? ''
         const codeHelper = cell.lastElementChild?.textContent?.trim()
-        let code : string | null = null 
+        let code: string | null = null
         if (codeHelper !== undefined) {
-          code = codeHelper.substring(1, codeHelper.length - 1) //  // remove square brackets
+          code = codeHelper.substring(1, codeHelper.length - 1) // remove square brackets
         }
-  
+
         return { name, code }
       }
       default: {
@@ -61,6 +67,6 @@ export class Usos6_8_1Handler extends Usos6_8_0Handler {
   }
 
   protected getLinkage(): Promise<Linkage[]> {
-      return new LinkageGetter(this.cellToSubject.bind(this)).getLinkage()
+    return new LinkageGetter(this.cellToSubject.bind(this)).getLinkage()
   }
 }
