bf4fbe5552ede48ae86ff1192447ac23825d9ca5
Hubert Badocha <badocha.hubert@gmail.com>
2023-02-20 23:21:17 +0100

    Fixed recrutation avgs

--- src/content/avgHandlers.ts
+++ src/content/avgHandlers.ts
@@ -42,7 +42,7 @@ function handleProgramToGrade(
   for (const pName of programs) {
     const matchingLinkages = linkages.filter(
       ({ program, includeInProgram }) =>
-        program.name === pName && includeInProgram,
+        includeInProgram && program.name === pName,
     )
     const matchingCodes = matchingLinkages.map(({ subject: { code } }) => code)
 
@@ -70,20 +70,40 @@ function handleMimSpecific(grades: Grade[]): AvgData[] {
 function handleAverageForMimMathMaster(grades: Grade[]) {
   return handleAverageForRecrutationByCodes(
     [
-      '1000-111bAM1', // analiza matematyczna I.1
-      '1000-112bAM2', // analiza matematyczna I.2
-      '1000-113bAM3', // analiza matematyczna II.1
-      '1000-114bAM4', // analiza matematyczna II.2
-      '1000-111bGA1', // geometria z algebrą liniową I
-      '1000-112bGA2', // geometria z algebrą liniową II
-      '1000-111bWMA', // wstęp do matematyki
-      '1000-111bWI1', // wstęp do informatyki
-      '1000-112bWI2', // wstęp do informatyki II
-      '1000-113bAG1', // algebra I
-      '1000-113bTP1', // topologia I
-      '1000-114bMOB', // matematyka obliczeniowa
-      '1000-114bRRZ', // równania różniczkowe zwyczajne
-      '1000-114bRP1', // rachunek prawdopodobieństwa I
+      '1000-111bAM1a', // analiza matematyczna I.1
+      '1000-111bAM1b', // analiza matematyczna I.1
+      '1000-112bAM2a', // analiza matematyczna I.2
+      '1000-112bAM2b', // analiza matematyczna I.2
+      '1000-112bAM2*', // analiza matematyczna I.2
+      '1000-113bAM3a', // analiza matematyczna II.1
+      '1000-113bAM3*', // analiza matematyczna II.1
+      '1000-114bAM4a', // analiza matematyczna II.2
+      '1000-114bAM4*', // analiza matematyczna II.2
+      '1000-111bGA1a', // geometria z algebrą liniową I
+      '1000-111bGA1b', // geometria z algebrą liniową I
+      '1000-112bGA2*', // geometria z algebrą liniową II
+      '1000-112bGA2a', // geometria z algebrą liniową II
+      '1000-112bGA2b', // geometria z algebrą liniową II
+      '1000-111ADM1', // Algebra dla MSEM I 
+      '1000-112ADM2', // Algebra dla MSEM I 
+      '1000-111bWMAa', // wstęp do matematyki
+      '1000-111bWMAb', // wstęp do matematyki
+      '1000-211bPM', // podstawy matematyki
+      '1000-111bWI1a', // wstęp do informatyki
+      '1000-111bWI1b', // wstęp do informatyki
+      '1000-112bWI2a', // wstęp do informatyki II
+      '1000-112bWI2b', // wstęp do informatyki II
+      '1000-113bAG1a', // algebra I
+      '1000-113bAG1*', // algebra I
+      '1000-113bTP1a', // topologia I
+      '1000-113bTP1*', // topologia I
+      '1000-215bMNU', // metody numeryczne
+      '1000-114bMOBa', // matematyka obliczeniowa
+      '1000-114bMOB*', // matematyka obliczeniowa
+      '1000-114bRRZa', // równania różniczkowe zwyczajne
+      '1000-114bRRZb', // równania różniczkowe zwyczajne
+      '1000-114bRP1a', // rachunek prawdopodobieństwa I
+      '1000-114bRP1*', // rachunek prawdopodobieństwa I
       '1000-134FAN', // funkcje analityczne
     ],
     'Średnia rekrutacyjna na S2-MAT (MIM UW)',
@@ -95,10 +115,23 @@ function handleAverageForMimCsMaster(grades: Grade[]) {
   return handleAverageForRecrutationByCodes(
     [
       '1000-211bAM1', // analiza matematyczna I
-      '1000-212bAM2', // analiza matematyczna II
+      '1000-111bAM1a', // analiza matematyczna I.1
+      '1000-111bAM1b', // analiza matematyczna I.1
+      '1000-112bAM2a', // analiza matematyczna I.2
+      '1000-112bAM2b', // analiza matematyczna I.2
+      '1000-112bAM2*', // analiza matematyczna I.2
       '1000-211bGAL', // geometria z algebrą liniową
+      '1000-111bGA1a', // geometria z algebrą liniową I
+      '1000-111bGA1b', // geometria z algebrą liniową I
+      '1000-111ADM1', // Algebra dla MSEM I 
       '1000-211bPM', // podstawy matematyki
-      '1000-211bWP', // wstęp do programowania
+      '1000-111bWMAa', // wstęp do matematyki
+      '1000-111bWMAb', // wstęp do matematyki
+      '1000-212bAM2', // analiza matematyczna II
+      '1000-113bAM3a', // analiza matematyczna II.1
+      '1000-113bAM3*', // analiza matematyczna II.1
+      '1000-211bWPI', // wstęp do programowania
+      '1000-211bWPF', // wstęp do programowania
       '1000-212bMD', // matematyka dyskretna
       '1000-212bPO', // programowanie obiektowe
       '1000-222bIPP', // indywidualny projekt programistyczny
@@ -106,14 +139,22 @@ function handleAverageForMimCsMaster(grades: Grade[]) {
       '1000-2N09ZBD', // bazy danych
       '1000-213bPW', // programowanie współbieżne
       '1000-214bSOB', // systemy operacyjne
+      '1000-214b', // systemy operacyjne
       '1000-214bSIK', // sieci komputerowe
       '1000-214bWWW', // aplikacje WWW
       '1000-214bJAO', // języki, automaty i obliczenia
       '1000-214bIOP', // inżynieria oprogramowania
       '1000-213bRPS', // rachunek prawdopodobieństwa i statystyka
+      '1000-114bRP1', // Rachunek prawdopodobieństwa I
+      '1000-114bRP1*', // Rachunek prawdopodobieństwa I
+      '1000-135ST1', // Statystyka I
+      '1000-115ST1', // Statystyka I 
+      '1000-714SAD', // Statystyczna analiza danych
       '1000-223bJNP1', // języki i narzędzia programowania I
       '1000-215bBSK', // bezpieczeństwo systemów komputerowych
       '1000-215bMNU', // metody numeryczne
+      '1000-114bMOBa', // Matematyka obliczeniowa
+      '1000-114bMOB*', // Matematyka obliczeniowa
       '1000-215bSWP', // semantyka i weryfikacja programów
     ],
     'Średnia rekrutacyjna na S2-INF (MIM UW)',
@@ -127,24 +168,15 @@ function handleAverageForRecrutationByCodes(
   grades: Grade[],
 ): AvgData[] {
   const gradesToCount: Grade[][] = []
-  let any = false
   for (const c of codes) {
     const cGrades = grades.filter(
-      ({ subject: { code } }) => code !== null && code.startsWith(c),
+      ({ subject: { code } }) => code === c,
     )
-    if (cGrades.length === 0) {
-      gradesToCount.push([getDeansTwoForCode(c)])
-    } else {
+    if (cGrades.length > 0) {
       gradesToCount.push(cGrades)
-      any = true
     }
   }
 
-  if (!any) {
-    return []
-  }
-
-  const gradesFilter = () => true
   const sameCodeGradeAction = (grades: Grade[]) => {
     if (grades.length === 0) {
       return []
@@ -156,27 +188,14 @@ function handleAverageForRecrutationByCodes(
   }
   const avg = new MaxAverageCounter().getAverage(
     gradesToCount.flat(),
-    gradesFilter,
+    undefined,
     sameCodeGradeAction,
   )
 
-  return [{ avg, label }]
-}
-
-function getDeansTwoForCode(code: string): Grade {
-  return {
-    grades: [
-      {
-        initialGrade: null,
-        finalGrade: null,
-        name: null,
-        isDeansTwo: true,
-      },
-    ],
-    subject: { name: '', code },
-    programs: [],
-    period: '',
+  if (avg.isNaN()) {
+    return []
   }
+  return [{ avg, label }]
 }
 
 function handleProgramStageToGrade(
